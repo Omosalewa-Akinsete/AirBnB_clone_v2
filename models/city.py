78353d3a7da422046664c418694eb5e0a3256b01
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-20 19:03:41 -0700

    AirBnB_clone_v2

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     if storage_type == "db":
