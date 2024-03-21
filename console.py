3a2301e2a33249c5dfd4f5c46cdc1f55e2eaa2e8
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-20 18:06:23 -0700

    Console improvements

--- console.py
+++ console.py
@@ -74,14 +74,12 @@ class HBNBCommand(cmd.Cmd):
                 if pline:
                     # check for *args or **kwargs
 
-                    if pline[0] == '{' and pline[-1] == '}'\
-
-                    if pline[0] is '{' and pline[-1] is '}'\
-
-                            and type(eval(pline)) is dict:
-                        _args = pline
-                    else:
-                        _args = pline.replace(',', '')
+                    if pline[0] == '{' and pline[-1] == '}':
+                        if pline[0] is '{' and pline[-1] is '}' \
+                                and type(eval(pline)) is dict:
+                            _args = pline
+                        else:
+                            _args = pline.replace(',', '')
                         # _args = _args.replace('\"', '')
             line = ' '.join([_cmd, _cls, _id, _args])
 
