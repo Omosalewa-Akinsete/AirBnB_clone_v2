6b739acfa753d679910bae880a29c44aeea79f56
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-23 09:39:02 -0700

    AirBnB_clone_v2

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ from sqlalchemy import String
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, PrimaryKey=True)
@@ -25,9 +26,10 @@ class BaseModel:
             if kwargs:
                 for key, value in kwargs.items():
                     if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f")
                     if key != "__class__":
-                        setattr(self, key, value) 
+                        setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -43,10 +45,13 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("sa_instance_state", None)
+        return my_dict
+
+    def delete(self):
+        """Deletes the current instance from the storage"""
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+import os 
 from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if genenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Gets a list of cities"""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+                return city_list
