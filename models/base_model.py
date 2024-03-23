0af75a56b191f412fe6da633590a560700100e2a
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-03-23 11:15:05 -0700

    AirBnB_clone_v2

--- models/base_model.py
+++ models/base_model.py
@@ -19,17 +19,15 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            if kwargs:
-                for key, value in kwargs.items():
-                    if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(
-                                value, "%Y-%m-%dT%H:%M:%S.%f")
-                    if key != "__class__":
-                        setattr(self, key, value)
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -38,10 +36,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        self.updated_at = datetime.utcnow()
+        model.storage.new(self)
+        model.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
