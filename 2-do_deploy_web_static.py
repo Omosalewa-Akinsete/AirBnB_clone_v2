bd2a7b66ef8ee5176ee4f58874e81b2c0d51dae3
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-04 15:56:22 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
 """Distributes an archive to your web servers, using the function do_deploy"""
-from os import path
+import os
 from fabric.api import *
 from datetime import datetime
 
 env.hosts = ['100.25.13.203', '34.201.61.187']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+    local("mkdir -p versions")
+    archived = local("tar -cvzf {} web_static".format(archive_path))
+    if archived.return_code != 0:
+        return None
+    else:
+        return archive_path
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    try:
-        if not (path.exists(archive_path)):
-            return False
-        put(archive_path, '/tmp/')
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-            .format(timestamp))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-    except SpecificException:
-        return False
-    return True
+    '''use os module to check for valid file path'''
+    if os.path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+        return True
+    return False
