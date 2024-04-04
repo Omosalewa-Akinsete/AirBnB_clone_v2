a7aa2eda7526ff29e20f7f5953f7c228bed98f68
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-04 12:22:33 -0700

    Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Deletes out-of-date archives, using the function do_clean
+"""Deletes out-of-date archives, using the function do_clean"""
 import os
 from fabric.api import *
 
@@ -9,10 +9,12 @@ env.hosts = ['100.25.13.203', '34.201.61.187']
 def do_clean(number=0):
     """Delete out-of-date archives"""
     number = 1 if int(number) == 0 else int(number)
+
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archives]
+
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
