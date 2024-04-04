2cda9d95dfb0a71aa24902fcb1e75f1efa02d8d4
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-04 13:57:37 -0700

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,20 +3,16 @@
 sudo apt-get update
 sudo apt-get install nginx
 sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/tests/
-sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
 sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html/
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a /n location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
