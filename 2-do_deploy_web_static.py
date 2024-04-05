09465c3001238e1c369ced09a6698b8b4074c343
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-04 17:09:23 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,43 +3,48 @@
 Distributes an archive to your web servers,
 using the function do_deploy
 """
-
 import os
-from fabric.api import *
+from fabric.api import task
+from fabric.api import local
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 from datetime import datetime
 
 
-# Set the host IP addresses for web-01 && web-02
-env.hosts = ['54.83.130.140', '18.235.255.48']
+env.hosts = ['100.25.13.203', '34.201.61.187']
 env.user = "ubuntu"
 
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(now)
-    local("mkdir -p versions")
-    archived = local("tar -cvzf {} web_static".format(archive_path))
-    if archived.return_code != 0:
-        return None
-    else:
-        return archive_path
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
 
 
 def do_deploy(archive_path):
     '''use os module to check for valid file path'''
-    if os.path.exists(archive_path):
-        archive = archive_path.split('/')[1]
-        a_path = "/tmp/{}".format(archive)
-        folder = archive.split('.')[0]
-        f_path = "/data/web_static/releases/{}/".format(folder)
-        put(archive_path, a_path)
-        run("mkdir -p {}".format(f_path))
-        run("tar -xzf {} -C {}".format(a_path, f_path))
-        run("rm {}".format(a_path))
-        run("mv -f {}web_static/* {}".format(f_path, f_path))
-        run("rm -rf {}web_static".format(f_path))
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(f_path))
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
         return True
-    return False
+    except Exception:
+        return False
