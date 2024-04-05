3ba004d1e088ffdd35fdf1b8739c8c67093b3564
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-04 18:26:15 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 Distributes an archive to your web servers,
 using the function do_deploy
 """
-import os
+from os import path
 from time import strftime
 from fabric.api import task
 from fabric.api import local
@@ -15,6 +15,7 @@ from datetime import datetime
 
 env.hosts = ['100.25.13.203', '34.201.61.187']
 env.user = "ubuntu"
+env.key_filename = '~/.ssh/school'
 
 
 def do_pack():
@@ -30,23 +31,29 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    '''use os module to check for valid file path'''
+    """Distributes an archive to your web servers"""
     try:
         if not os.path.exists(archive_path):
             return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
-        return True
-    except Exception:
+        put(archive_path, '/tmp/')
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
+            .format(timestamp))
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C '
+            '/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+        run('sudo rm /tmp/web_static_{}.tgz'
+            .format(timestamp))
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* '
+            '/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'
+            .format(timestamp))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s /data/web_static/releases/web_static_{}/ '
+            '/data/web_static/current'
+            .format(timestamp))
+    except Exception as e:
+        print(e)
         return False
+    return True
