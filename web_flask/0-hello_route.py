357cb8bfe04c801b4e10bdbce96156ebb73a1db3
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-18 16:04:55 -0700

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ app = Flask("__name__")
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """A route that displays Hello HBNB"""
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
