7d008ebc07d1b355b8490e5b30df6614567738e4
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-19 09:03:29 -0700

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,7 @@ def hbnb():
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
+def c_text(text):
     """A route that displays c text"""
     text = text.replace("_", " ")
     return "C {}".format(text)
