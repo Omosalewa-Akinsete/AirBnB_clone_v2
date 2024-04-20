c73e3555fba321f2ea30e9460a0dec8efb0cff70
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-20 07:15:32 -0700

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,21 +3,20 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-from moels.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_storage(exception):
-    """A route that closes the storage on teardown"""
-    storage.close()
-
-
-@app.route("/states_list")
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """A route that displays a HTML page with the list of State objects"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all("State")
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """A route that removes the current SQLAlchemy session"""
+    storage.close()
 
 
 if __name__ == "__main__":
