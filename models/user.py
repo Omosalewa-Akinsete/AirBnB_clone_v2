afc591c4f99988354e47e23ffe12dfd8791be3d5
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-20 13:42:18 -0700

    User

--- models/user.py
+++ models/user.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+"""Defines the User class"""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-import os
-
-
-storage_type = os.environ.get("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    if storage_type == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        reviews = relationship(
-                "Review",
-                cascade="all, delete, delete-orphan",
-                backref="user"
-                )
-        places = relationship(
-                "Place",
-                cascade="all, delete, delete-orphan",
-                backref="user"
-                )
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
